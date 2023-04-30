b523de29b421f7c1efc11e6c510c5ca33285156f
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-04-30 12:45:01 -0500

    update requeriments y pipeline

--- Parte2/api/prediction_model.py
+++ Parte2/api/prediction_model.py
@@ -3,7 +3,7 @@ from joblib import load
 class Model:
 
     def __init__(self):
-        self.model = load("Pipeline.joblib")
+        self.model = load("pipeline.joblib")
 
     def make_prediction(self, data):
         result = self.model.predict(data)
